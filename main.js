00468051bd9494ec8b2c85d4bb9c183759970dce
Karol Filas <filaskarol@gmail.com>
2023-04-10 10:42:22 +0200

    blinking cursor

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const spanText = document.querySelector('.text')
 const txt = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam distinctio officiis corrupti est quas consequuntur! Reiciendis, aliquid nihil provident quo ipsum et odit eum iusto rem, repellendus aliquam. Dolor, repellat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam distinctio officiis corrupti est quas consequuntur! Reiciendis, aliquid nihil provident quo ipsum et odit eum iusto rem, repellendus aliquam. Dolor, repellat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam distinctio officiis corrupti est quas consequuntur! Reiciendis, aliquid nihil provident quo ipsum et odit eum iusto rem, repellendus aliquam. Dolor, repellat?'
+const spanCursor = document.querySelector('.cursor')
 
 let indexText = 0
 
@@ -12,3 +13,9 @@ const addLetter = () => {
 }
 
 const indexTyping = setInterval(addLetter, 10)
+
+const cursorAnimation = () => {
+    spanCursor.classList.toggle('active');
+}
+
+setInterval(cursorAnimation, 400)
